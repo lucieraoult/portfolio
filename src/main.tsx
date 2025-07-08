f38d575df4fe9a8353d3e9c48f1f54f8c955336b
lucieraoult <lucieraoult@gmail.com>
2025-07-08 12:44:47 +0100

    Install PostHog tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,26 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.jsx'
+import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const isProd = import.meta.env.MODE === 'production'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+}
+
+if (!isProd) {
+  // Explicitly disable PostHog tracking in dev
+  posthog.opt_out_capturing()
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <PostHogProvider apiKey={isProd ? import.meta.env.VITE_PUBLIC_POSTHOG_KEY : undefined} options={options}>
+      <App />
+    </PostHogProvider>
+  </StrictMode>,
+);
